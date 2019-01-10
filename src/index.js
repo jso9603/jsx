76a747623fd3880fe837ac6a8fc56d043d40d2f4
JISOO PARK <jso9603@gmail.com>
2019-01-10 14:43:46 +0900

    Add label, input, and button using JSX

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+  const buttonText = { text: 'Click me'};
+  const labelText = 'Enter name: ';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  )
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    // index.html의 body부분의 id 참고
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 );
