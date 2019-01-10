4fc1c1c3e535bc7333a5c020e1970a84fc4c7e33
RA_Dev <ra_dev@RA-Devui-MacBook-Pro.local>
2019-01-10 12:05:18 +0900

    Initial commit about create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+const App = () => {
+    return <div>Hi there!</div>
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    // index.html의 body부분의 id 참고
+    document.querySelector('#root')
+);
